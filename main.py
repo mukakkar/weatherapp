a7f6b332eaf1e8544210e8b1c3438e4779025271
Munish Kakkar <mkakkar@caltex.com.au>
2019-04-10 21:49:04 +1000

    parser updates

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
 
     # Set up the logging for the program
     set_up_logging('./conf/logging.json')
-    #logger = logging.getLogger(__name__)
+    logger = logging.getLogger(__name__)
     cities = []
     samples_to_generate = 10
 
@@ -41,12 +41,12 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser("Weather Simulator")
     subparsers = parser.add_subparsers(help="Weather Simulation parser Help", dest="cmd")
-    # samples subparser and Arguments definition
+    # sample subparser and Arguments definition
     sp = subparsers.add_parser("sample", help='Command to generate weather sample based on user input ')
     sp.add_argument("samples", type=int, help="Number of Samples to generate should be < 1000 ")
     sp.add_argument("gapikey", help="Google API Key to fetch geo-coordinates")
     sp.add_argument("dapikey", help="DarkSky API Key to fetch ")
-    # fileloc subparser and Arguments definition
+    # csvfile subparser and Arguments definition
     sp = subparsers.add_parser("csvfile", help="Command to generate weather sample based on file contents")
     sp.add_argument("file", help="CSV file with cities and country information ")
     sp.add_argument("samples", type=int, help="Number of Samples to generate should be < 1000 ")
@@ -54,7 +54,7 @@ if __name__ == "__main__":
     sp.add_argument("dapikey", help="DarkSky API Key to fetch ")
     args = parser.parse_args()
 
-   # logger.info("Weather data generation starting now")
+    logger.info("Weather data generation starting now")
 
     if args.cmd == "sample":
         # Simulate the weather for 10 cities mentioned below
@@ -67,14 +67,14 @@ if __name__ == "__main__":
         # Simulate the weather for cities information fetched from the csv file
         with open(args.file) as fin:
             cities_reader=csv.reader(fin, delimiter=',', quotechar='\"')
-            cities = [tuple(row) for row in  cities_reader]
+            cities = [tuple(row) for row in cities_reader]
             samples_to_generate = args.samples
 
     gw = GenerateWeather(cities, args.gapikey, args.dapikey)
     records = gw.generate_weather(samples_to_generate)
 
-   # logger.info("Weather data generation finished . Staring the simulation now")
+    logger.info("Weather data generation finished . Staring the simulation now")
 
     sim = SimulateWeather(pd.DataFrame(records))
     sim.simulate(samples_to_generate)
-    sim.print_weather(**{"file": "./data/simulated_weather.txt"})
+    sim.print_weather(**{"file": "./data/simulated_weather.csv"})
